741137ff163dd5a448aedfb446fa83b8f79f1548
avargas20 <a.vargas20@uniandes.edu.co>
2021-02-04 12:27:56 -0500

    Se elimina linea redundante (Azul)

--- src/logica/coleccion.py
+++ src/logica/coleccion.py
@@ -158,11 +158,10 @@ class Coleccion():
     def buscar_canciones_por_interprete(self, interprete_nombre):
         if interprete_nombre == "":
             canciones = session.query(Cancion).all()
-            return canciones
         else:
             canciones = session.query(Cancion).filter(
                 Cancion.interpretes.any(Interprete.nombre.ilike('%{0}%'.format(interprete_nombre)))).all()
-            return canciones
+        return canciones
     
     def asociar_cancion(self, cancion_id, album_id):
         cancion = session.query(Cancion).filter(Cancion.id == cancion_id).first()
