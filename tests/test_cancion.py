f4dff7fd88b9072c851c7a5a23831bf25aa717bd
avargas20 <a.vargas20@uniandes.edu.co>
2021-02-04 15:11:53 -0500

    Generación de datos aleatorios en test_cancion.py

--- tests/test_cancion.py
+++ tests/test_cancion.py
@@ -1,5 +1,7 @@
 import unittest
 
+from faker import Faker
+
 from src.logica.coleccion import Coleccion
 from src.modelo.album import Album
 from src.modelo.cancion import Cancion
@@ -12,93 +14,128 @@ class CancionTestCase(unittest.TestCase):
     def setUp(self):
         self.session = Session()
         self.coleccion = Coleccion()
-
-    def test_dar_cancion_por_id(self):
-        cancion = self.session.query(Cancion).filter(Cancion.titulo == "Baby blues").first()
-        if cancion is None:
-            nuevo_album = Album(titulo="Amapola azul", ano=2020, descripcion="Instrumental", medio="CD")
-            nuevo_interprete = Interprete(nombre="Andrea Echeverri", texto_curiosidades="En ese año nacio su hijo...",
-                                          cancion=-1)
-            nueva_cancion = Cancion(titulo="Baby blues", minutos=3, segundos=20, compositor="Desconocido",
-                                    albumes=[nuevo_album])
-            nueva_cancion.interpretes.append(nuevo_interprete)
-            nuevo_interprete.cancion = nueva_cancion.id
-            self.session.add(nuevo_album)
-            self.session.add(nuevo_interprete)
-            self.session.add(nueva_cancion)
-            self.session.commit()
-            consulta = self.coleccion.dar_cancion_por_id(nueva_cancion.id)
-        else:
-            consulta = self.coleccion.dar_cancion_por_id(cancion.id)
-        self.assertIsNotNone(consulta)
-
-    def test_buscar_canciones_por_titulo(self):
-        cancion = self.session.query(Cancion).filter(Cancion.titulo == "Express Yourself").first()
-        if cancion is None:
-            nuevo_album = Album(titulo="Like a Prayer", ano=1989, descripcion="Sin descripción", medio="CASETE")
-            nuevo_interprete = Interprete(nombre="Madona",
-                                          texto_curiosidades="Publicado por la compañía discográfica Sire Records",
-                                          cancion=-1)
-            nueva_cancion = Cancion(titulo="Express Yourself", minutos=4, segundos=39,
-                                    compositor="Stephen Bray y otros",
-                                    albumes=[nuevo_album])
-            nueva_cancion.interpretes.append(nuevo_interprete)
-            nuevo_interprete.cancion = nueva_cancion.id
-            self.session.add(nuevo_album)
-            self.session.add(nuevo_interprete)
-            self.session.add(nueva_cancion)
-            self.session.commit()
-        consulta = self.coleccion.buscar_canciones_por_titulo("Expr")
-        self.assertEqual(len(consulta), 1)
-
-    def test_editar_cancion(self):
-        cancion = self.session.query(Cancion).filter(Cancion.titulo == "Express Yourself").first()
-        interprete = self.session.query(Interprete).filter(Interprete.nombre == "Madona").first()
-        if cancion is not None:
-            self.coleccion.editar_cancion(cancion.id, "Express Yourself", 2, 54, "Patrick Leonard y otros",
-                                          [{'id': interprete.id, 'nombre': 'Madona',
-                                            'texto_curiosidades': 'Publicado por la compañía discográfica Sire Records'}])
-            consulta = self.session.query(Cancion).filter(Cancion.compositor == "Patrick Leonard y otros").first()
-            self.assertIsNotNone(consulta)
-
-    def test_eliminar_cancion(self):
-        self.coleccion.eliminar_cancion(1)
-        consulta = self.session.query(Cancion).filter(Cancion.id == 1).first()
-        self.assertIsNone(consulta)
+        # Generación de datos con libreria Faker
+        self.data_factory = Faker()
 
     def test_cancion_sin_interpretes(self):
-        cancion = self.coleccion.agregar_cancion("Felicidad", 3, 10, "Desconocido", -1, [])
+        # Nombre aleatorio
+        titulo_cancion = self.data_factory.name()
+        # Número aleatorio entre 0 y 60
+        minutos_cancion = self.data_factory.pyint(0, 60)
+        segundos_cancion = self.data_factory.pyint(0, 60)
+        compositor_cancion = self.data_factory.name()
+        cancion = self.coleccion.agregar_cancion(titulo_cancion, minutos_cancion, segundos_cancion, compositor_cancion, -1, [])
         self.assertEqual(cancion, False)
 
     def test_cancion_varios_interpretes(self):
-        self.coleccion.agregar_interprete("Vicente Fernandez", "Grabado en 3 potrillos", -1)
-        self.coleccion.agregar_interprete("Alejandro Fernandez", "En honor al aniversario...", -1)
-        self.coleccion.agregar_cancion("Felicidad", 4, 37, "Desconocido", -1,
-                                       [{'nombre': 'Vicente Fernandez', 'texto_curiosidades': 'Grabado en 3 potrillos'},
-                                        {'nombre': 'Alejandro Fernandez',
-                                         'texto_curiosidades': 'En honor al aniversario...'}])
-        consulta = self.session.query(Cancion).filter(Cancion.titulo == "Felicidad").first()
+        nombre_interprete1 = self.data_factory.name()
+        # Frase aleatoria
+        texto_curiosidades1 = self.data_factory.sentence()
+        self.coleccion.agregar_interprete(nombre_interprete1, texto_curiosidades1, -1)
+        nombre_interprete2 = self.data_factory.name()
+        texto_curiosidades2 = self.data_factory.sentence()
+        self.coleccion.agregar_interprete(nombre_interprete2, texto_curiosidades2, -1)
+        titulo_cancion = self.data_factory.name()
+        minutos_cancion = self.data_factory.pyint(0, 60)
+        segundos_cancion = self.data_factory.pyint(0, 60)
+        compositor_cancion = self.data_factory.name()
+        self.coleccion.agregar_cancion(titulo_cancion, minutos_cancion, segundos_cancion, compositor_cancion, -1,
+                                       [{'nombre': nombre_interprete1, 'texto_curiosidades': texto_curiosidades1},
+                                        {'nombre': nombre_interprete2,
+                                         'texto_curiosidades': texto_curiosidades2}])
+        consulta = self.session.query(Cancion).filter(Cancion.titulo == titulo_cancion).first()
         self.assertIsNotNone(consulta)
 
     def test_cancion_con_album(self):
-        self.coleccion.agregar_album("Renacer", 2005, "Sin descripción", "CD")
-        consulta1 = self.session.query(Album).filter(Album.titulo == "Renacer").first().id
+        titulo_album = self.data_factory.name()
+        # Año aleatorio
+        anio_album = self.data_factory.year()
+        descripcion_album = self.data_factory.sentence()
+        self.coleccion.agregar_album(titulo_album, anio_album, descripcion_album, "CD")
+        consulta1 = self.session.query(Album).filter(Album.titulo == titulo_album).first().id
         self.coleccion.agregar_interprete("Alejandra Guzman", "Canción dedicada a su ...", -1)
-        self.coleccion.agregar_cancion("Bye mamá", 1, 48, "Desconocido", consulta1,
+        minutos_cancion = self.data_factory.pyint(0, 60)
+        segundos_cancion = self.data_factory.pyint(0, 60)
+        compositor_cancion = self.data_factory.name()
+        self.coleccion.agregar_cancion("Bye mamá", minutos_cancion, segundos_cancion, compositor_cancion, consulta1,
                                        [{'nombre': 'Alejandra Guzman',
                                          'texto_curiosidades': 'Canción dedicada a su ...'}])
         consulta2 = self.session.query(Cancion).filter(Cancion.titulo == "Bye mamá").first()
         self.assertNotEqual(len(consulta2.albumes), 0)
 
     def test_cancion_repetida_album(self):
-        self.coleccion.agregar_album("25", 2015, "Sin descripción", "CD")
-        consulta1 = self.session.query(Album).filter(Album.titulo == "25").first().id
-        self.coleccion.agregar_interprete("Adele", "Premio Grammy a la mejor grabación del año", -1)
-        cancion1 = self.coleccion.agregar_cancion("Hello", 3, 45, "Desconocido", consulta1,
-                                                  [{'nombre': 'Adele',
-                                                    'texto_curiosidades': 'Premio Grammy a la mejor grabación del año'}])
-        self.coleccion.agregar_interprete("Queen", "Premio Grammy a la mejor grabación del año", -1)
-        cancion2 = self.coleccion.agregar_cancion("Hello", 5, 55, "Desconocido", consulta1,
-                                                  [{'nombre': 'Queen',
-                                                    'texto_curiosidades': 'Canción demasiado larga para la época'}])
-        self.assertEqual(cancion2, False)
+        titulo_album = self.data_factory.name()
+        anio_album = self.data_factory.year()
+        descripcion_album = self.data_factory.sentence()
+        self.coleccion.agregar_album(titulo_album, anio_album, descripcion_album, "CD")
+        consulta1 = self.session.query(Album).filter(Album.titulo == titulo_album).first().id
+        nombre_interprete1 = self.data_factory.name()
+        # Texto aleatorio
+        texto_curiosidades1 = self.data_factory.text()
+        self.coleccion.agregar_interprete(nombre_interprete1, texto_curiosidades1, -1)
+        titulo_cancion1 = self.data_factory.name()
+        minutos_cancion1 = self.data_factory.pyint(0, 60)
+        segundos_cancion1 = self.data_factory.pyint(0, 60)
+        compositor_cancion1 = self.data_factory.name()
+        self.coleccion.agregar_cancion(titulo_cancion1, minutos_cancion1, segundos_cancion1, compositor_cancion1, consulta1,
+                                                       [{'nombre': nombre_interprete1,
+                                                         'texto_curiosidades': texto_curiosidades1}])
+        nombre_interprete2 = self.data_factory.name()
+        texto_curiosidades2 = self.data_factory.text()
+        self.coleccion.agregar_interprete(nombre_interprete2, texto_curiosidades2, -1)
+        minutos_cancion2 = self.data_factory.pyint(0, 60)
+        segundos_cancion2 = self.data_factory.pyint(0, 60)
+        compositor_cancion2 = self.data_factory.name()
+        cancion = self.coleccion.agregar_cancion(titulo_cancion1, minutos_cancion2, segundos_cancion2, compositor_cancion2, consulta1,
+                                                       [{'nombre': nombre_interprete2,
+                                                         'texto_curiosidades': texto_curiosidades2}])
+        self.assertEqual(cancion, False)
+
+    def test_editar_cancion(self):
+        consulta1 = self.session.query(Cancion).filter(Cancion.id == 2).first().compositor
+        consulta2 = self.session.query(Interprete).filter(Interprete.nombre == "Franco de Vita").first()
+        texto_curiosidades = self.data_factory.text()
+        minutos_cancion = self.data_factory.pyint(0, 60)
+        segundos_cancion = self.data_factory.pyint(0, 60)
+        if consulta2 is None:
+            self.coleccion.agregar_interprete("Franco de Vita", texto_curiosidades, 1)
+            self.coleccion.editar_cancion(2, "Bye mamá", minutos_cancion, segundos_cancion, "J.R.Florez y Difelisatti",
+                                          [{'id': '2', 'nombre': 'Alejandra Guzman',
+                                            'texto_curiosidades': 'Canción dedicada a su ...'},
+                                           {'id': 'n', 'nombre': 'Franco de Vita',
+                                            'texto_curiosidades': texto_curiosidades}])
+        else:
+            self.coleccion.editar_cancion(2, "Bye bye", minutos_cancion, segundos_cancion, "J.R.Florez y Difelisatti",
+                                          [{'id': '2', 'nombre': 'Alejandra Guzman',
+                                            'texto_curiosidades': 'Canción dedicada a su ...'},
+                                           {'id': '9', 'nombre': 'Franco de Vita',
+                                            'texto_curiosidades': texto_curiosidades}])
+        consulta3 = self.session.query(Cancion).filter(Cancion.id == 2).first()
+        self.assertEqual(consulta3.compositor, "J.R.Florez y Difelisatti")
+
+    def test_eliminar_cancion(self):
+        self.coleccion.eliminar_cancion(3)
+        consulta = self.session.query(Cancion).filter(Cancion.id == 3).first()
+        self.assertIsNone(consulta)
+
+    def test_buscar_canciones_por_titulo(self):
+        titulo_cancion = self.data_factory.name()
+        anio_cancion = self.data_factory.year()
+        descripcion_cancion = self.data_factory.sentence()
+        self.coleccion.agregar_album(titulo_cancion, anio_cancion, descripcion_cancion, "CD")
+        consulta1 = self.session.query(Album).filter(Album.titulo == titulo_cancion).first().id
+        nombre_interprete = self.data_factory.name()
+        texto_curiosidades = self.data_factory.text()
+        self.coleccion.agregar_interprete(nombre_interprete, texto_curiosidades, -1)
+        minutos_cancion = self.data_factory.pyint(0, 60)
+        segundos_cancion = self.data_factory.pyint(0, 60)
+        compositor_cancion = self.data_factory.name()
+        self.coleccion.agregar_cancion("Baby blues", minutos_cancion, segundos_cancion, compositor_cancion, consulta1,
+                                       [{'nombre': nombre_interprete,
+                                         'texto_curiosidades': texto_curiosidades}])
+        consulta = self.coleccion.buscar_canciones_por_titulo("Baby")
+        self.assertGreater(len(consulta), 0)
+
+    def test_dar_cancion_por_id(self):
+        consulta = self.coleccion.dar_cancion_por_id(1)
+        self.assertEqual(consulta["titulo"], "Baby blues")
