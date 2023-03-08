20dd8e8a0cea376ec2797dbe3690f32687996c86
mamunozb <ma.munozb1@uniandes.edu.co>
2023-03-08 18:57:24 -0500

    Actualización test

--- tests/test_album.py
+++ tests/test_album.py
@@ -35,7 +35,7 @@ class AlbumTestCase(unittest.TestCase):
 
     def test_editar_album(self):
         nueva_descripcion_album = self.data_factory.sentence()
-        self.coleccion.editar_album(2, "Clara luna-Mix", 2013, nueva_descripcion_album, "DISCO")
+        self.coleccion.editar_album(2, "Clara luna-Mix1", 2013, nueva_descripcion_album, "DISCO")
         consulta = self.session.query(Album).filter(Album.id == 2).first()
         self.assertIsNot(consulta.titulo, "Live Killers")
 
